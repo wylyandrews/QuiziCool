7a046c044458c9c5072e493a9e6482e7063c1dad
sirdude103 <sirdude103@gmail.com>
2019-04-04 23:50:32 -0500

    General editing, made username and category name unique, added at least one test entry for every table, automatically drops tables when inserting new test data

--- Database_Init.sql
+++ Database_Init.sql
@@ -5,47 +5,48 @@
 -- Created on:			March 28, 2019					--
 -- Last updated on:		March 28, 2019					--
 
-DROP TABLE questions;
+DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
 questionID SERIAL NOT NULL PRIMARY KEY,
 content TEXT NOT NULL,
-answer VARCHAR(1) NOT NULL,
+answer VARCHAR(1) NOT NULL,  CONSTRAINT answer_choices CHECK (answer IN ('A', 'B', 'C', 'D')),
 categoryID INT NOT NULL,
 difficultyLevel INT NOT NULL
 );
 
-DROP TABLE questionChoices;
+DROP TABLE IF EXISTS questionChoices;
 CREATE TABLE questionChoices (
 questionID INT NOT NULL,
-letter CHAR(1) NOT NULL,
+letter CHAR(1) NOT NULL, CONSTRAINT letter_choices CHECK (letter IN ('A', 'B', 'C', 'D')),
 choice TEXT NOT NULL,
 CONSTRAINT PK_Choice PRIMARY KEY(questionID,letter)
 );
 
-DROP TABLE difficulties;
+DROP TABLE IF EXISTS difficulties;
 CREATE TABLE difficulties (
 difficultyLevel INT NOT NULL PRIMARY KEY,
 score INT NOT NULL
 );
 
-DROP TABLE categories;
+DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
 categoryID SERIAL NOT NULL PRIMARY KEY,
+categoryName TEXT NOT NULL UNIQUE,
 creatorID INT NOT NULL,
-catDescription TEXT NOT NULL,
+categoryDescription TEXT,
 totalScore INT NOT NULL
 );
 
-DROP TABLE players;
+DROP TABLE IF EXISTS players;
 CREATE TABLE players (
 playerID SERIAL NOT NULL PRIMARY KEY,
-username VARCHAR(20) NOT NULL,
+username VARCHAR(20) NOT NULL UNIQUE,
 score INT NOT NULL DEFAULT 0,
 correctAnswers INT NOT NULL DEFAULT 0,
 totalAnswers INT NOT NULL DEFAULT 0
 );
 
-DROP TABLE scores;
+DROP TABLE IF EXISTS scores;
 CREATE TABLE scores (
 playerID INT NOT NULL,
 categoryID INT NOT NULL,
--- Test_Inserts.sql
+++ Test_Inserts.sql
@@ -5,5 +5,60 @@
 -- Created on:			March 28, 2019					--
 -- Last updated on:		March 28, 2019					--
 
+DELETE FROM questions;
+DELETE FROM questionChoices;
+DELETE FROM difficulties;
+DELETE FROM categories;
+DELETE FROM players;
+DELETE FROM scores;
+
 INSERT INTO players ( username, score, correctAnswers, totalAnswers) 
 			 VALUES ( 'QuizBot', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO players ( username ) 
+			 VALUES ( 'QuizBot2');
+
+
+INSERT INTO difficulties( difficultyLevel, score )
+			 	 VALUES ( 0, 0 );
+
+INSERT INTO difficulties( difficultyLevel, score )
+			 	 VALUES ( 1, 10 );
+
+INSERT INTO difficulties( difficultyLevel, score )
+			 	 VALUES ( 2, 20 );
+
+-- should fail, no duplicate difficultyLevel --
+INSERT INTO difficulties( difficultyLevel, score )
+			 	 VALUES ( 1, 5 );
+-- end fail --
+
+INSERT INTO categories ( categoryName, creatorID, categoryDescription, totalScore )
+				VALUES ( 'Test Category', (SELECT playerID FROM players WHERE username = 'QuizBot'), 'Good luck on your quiz!', 100 );
+
+INSERT INTO questions ( content, answer, categoryID, difficultyLevel )
+			   VALUES ( 'Which choice is choice B?', 'B', (SELECT categoryID FROM categories WHERE categoryName = 'Test Category'), 0 );
+
+INSERT INTO questionChoices ( questionID, letter, choice )
+					 VALUES ( 1, 'A', 'Option A' );
+
+INSERT INTO questionChoices ( questionID, letter, choice )
+					 VALUES ( 1, 'B', 'Choice B' );
+
+INSERT INTO questionChoices ( questionID, letter, choice )
+					 VALUES ( 1, 'C', 'Selection C' );
+
+INSERT INTO questionChoices ( questionID, letter, choice )
+					 VALUES ( 1, 'D', 'Possibility D' );
+
+-- should fail, no option E allowed --
+INSERT INTO questionChoices ( questionID, letter, choice )
+					 VALUES ( 1, 'E', 'Error E' );
+-- end fail --
+
+-- right now, this works, but we should implement constraints to make this fail ( invalid total answers, too many ) --
+INSERT INTO scores ( playerID, categoryID, score, correctAnswers, totalAnswers )
+			VALUES ( (SELECT playerID FROM players WHERE username = 'QuizBot'),
+					 (SELECT categoryID FROM categories WHERE categoryName = 'Test Category'),
+					 100, 10, 10 );
+
