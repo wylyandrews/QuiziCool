f96a45fa22c0a3170e1ec464b4f72371fcc02ff6
Wyly Andrews <31495312+sirdude103@users.noreply.github.com>
2019-04-12 01:22:03 -0500

    minor directory name fix

--- Database/setup_database.sql
+++ Database/setup_database.sql
@@ -5,7 +5,7 @@
 -- Created on:       April 11, 2019                    --
 -- Last updated on:	 April 12, 2019                    --
 
-\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Database_Init.sql';
-\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Stored_Procedures.sql';
-\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Test_Inserts.sql';
-\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Test_Procedure_Calls.sql';
+\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Database/Database_Init.sql';
+\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Database/Stored_Procedures.sql';
+\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Database/Test_Inserts.sql';
+\i 'E:/Users/gandr/Documents/GitHub/QuiziCool/Database/Test_Procedure_Calls.sql';
