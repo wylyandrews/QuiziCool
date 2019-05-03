c9226e9aa65ede7ee0f8a0d0aa66e3df4787ae2f
Dawson Coleman <dawsonmcoleman@gmail.com>
2019-05-02 19:16:53 -0500

    Finish addscore.php

--- Backend/addscore.php
+++ Backend/addscore.php
@@ -2,14 +2,30 @@
 
 include 'db.php';
 
-$playerid = $_GET['playerid'];
+$username = $_GET['username'];
 $categoryid = $_GET['categoryid'];
 $score = $_GET['score'];
 $numcorrect = $_GET['numcorrect'];
 
+//get player id
+$playerid = intval(pg_fetch_row(
+    pg_query_params($db, "SELECT * FROM players WHERE username = $1", array($username))
+)[0]);
+print pg_last_error();
+
+//Make a new user if they don't exist
+if($playerid == NULL) {
+    $playerid = intval(pg_fetch_row(
+        pg_query_params($db, "INSERT INTO players (username) VALUES ($1) RETURNING playerid", array($username))
+    )[0]);
+}
+print pg_last_error();
+
+//Add the score
 $result = pg_query_params(
     $db, 'CALL new_score($1, $2, $3, $4)',
     array($playerid, $categoryid, $score, $numcorrect)
 );
+print pg_last_error();
 
 ?>
